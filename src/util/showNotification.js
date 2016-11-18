0b1e7c36b97050044d534b6b438d587ba6952afc
Juan C. Olivares <cristobal@cxsoftware.com>
2016-11-18 17:26:34 -0300

    Getting video title before launching the notification

--- src/util/showNotification.js
+++ src/util/showNotification.js
@@ -4,19 +4,29 @@ const path = require ('path');
 
 // Local
 const play = require ('./play');
+const videoinfo = require ('./videoinfo');
 
 // Constants
 const ICON_PATH = path.join (__dirname, '..', '..', 'images', 'play.png');
 
-module.exports = (url) =>
-	libnotify.createNotification ({
-		summary: 'YouTube',
-		body: url,
-		icon: ICON_PATH,
-		actions: {
-			play: {
-				label: 'Play',
-				callback: () => play (url)
+module.exports = url =>
+{
+	(async () =>
+	{
+		const info = await videoinfo (url);
+		libnotify.createNotification ({
+			summary: 'YouTube',
+			body: info.title,
+			icon: ICON_PATH,
+			actions: {
+				play: {
+					label: 'Play',
+					callback: () => play (url)
+				},
+				close: {
+					callback: () => { notification = null }
+				}
 			}
-		}
-	}).push ();
+		}).push ();
+	})();
+};
