aeed74fe32ef17dfb124df2507a7edf3487dc240
Juan C. Olivares <cristobal@cxsoftware.com>
2016-11-21 17:34:06 -0300

    Adding download button to notification Don't open the second notification if the first one was already closed

--- src/util/showNotification.js
+++ src/util/showNotification.js
@@ -3,6 +3,7 @@ const notifications = require ('freedesktop-notifications');
 const path = require ('path');
 
 // Local
+const download = require ('./download');
 const play = require ('./play');
 const videoinfo = require ('./videoinfo');
 
@@ -13,9 +14,11 @@ module.exports = url =>
 {
 	(async () =>
 	{
+		var closed = false;
 		const actionCallback = a =>
 		{
 			if (a == 'play') play (url);
+			if (a == 'download') download (url);
 		};
 		const data = {
 			summary: 'YouTube' ,
@@ -23,16 +26,20 @@ module.exports = url =>
 			icon: ICON_PATH,
 			actions: {
 				default: '',
-				play: 'Play'
+				play: 'Play',
+				download: 'Download'
 			}
 		};
 		const notification1 = notifications
 			.createNotification (data)
 			.on ('action', actionCallback)
+			.on ('close', () => closed = true)
 			.push ();
 
 		// Get video info
 		const info = await videoinfo (url);
+		if (closed)
+			return;
 
 		// Create new notification
 		data.body = info.title;
