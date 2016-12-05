7cba02171ca321cd498926868e3caf35f21bc3d0
Juan C. Olivares <cristobal@cxsoftware.com>
2016-12-05 00:07:24 -0300

    Adding new gulp task: lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require ('gulp');
 const babel = require ('gulp-babel');
+const eslint = require ('gulp-eslint');
 
-gulp.task ('default', () =>
+gulp.task ('lint', () =>
+	gulp
+		.src ('src/**')
+		.pipe (eslint ())
+		.pipe (eslint.format ())
+		.pipe (eslint.failOnError()));
+
+gulp.task ('build', ['lint'], () =>
 	gulp
 		.src ('src/**/*.js')
 		.pipe (babel({
 			presets: ['es2015', 'stage-3']
 		}))
 		.pipe (gulp.dest ('dist')));
+
+gulp.task ('default', ['build']);
